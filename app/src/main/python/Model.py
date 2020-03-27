fa04869ec3d766745565f5effa103f3dc08bc27f
Susmit-A <susmit600@gmail.com>
2020-03-27 19:55:52 +0530

    Pick layers with index 3 and 5 instead of picking random layers.

--- app/src/main/python/Model.py
+++ app/src/main/python/Model.py
@@ -6,7 +6,7 @@ class Model:
     def __init__(self, weights_path):
         self.model = tf.keras.models.load_model(weights_path)
         self.model = tf.keras.Model(inputs=self.model.inputs,
-                                    outputs=random.sample(self.model.outputs, 2))
+                                    outputs=[self.model.outputs[3], self.model.outputs[5]])
 
     def dream(self, context, array, steps, step_size):
         return dream.dream(self.model, context, array, steps, step_size)
